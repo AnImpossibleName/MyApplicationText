5607d6c35daab2ceb95d5e8425f9e29196391ee1
AnImpossibleName <YztForWork@163.com>
2023-06-29 14:38:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationtext/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplicationtext;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Testt(View view){
+
+        Toast.makeText(this,"测试成功",Toast.LENGTH_SHORT).show();
+    }
+}
